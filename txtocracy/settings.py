c47e17cf96e7aff5ca93f1ba2609ff0ad894532f
Albert O'Connor <amjoconn@gmail.com>
2012-08-26 22:01:00 -0400

    Log it.

--- txtocracy/settings.py
+++ txtocracy/settings.py
@@ -156,7 +156,6 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
         }
     },
     'loggers': {
